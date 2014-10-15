d3741c4fe0bec343a5b0ac7731c1846fbb710450
Yoji Ueda <bluezbz@gmail.com>
2014-10-15 21:58:07 +0900

    Initial Commit

---
+++ APISFeedSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APISFeedSample
+//
+//  Created by 植田 洋次 on 2014/10/15.
+//  Copyright (c) 2014年 Yoji Ueda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
